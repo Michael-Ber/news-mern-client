80fc0caf54426f0c5a2045a4a305e418af8a9c18
Бершадский Михаил Сергеевич <Michail.Bershadsky@ilimtimber.corp>
2023-10-12 12:45:43 +0800

    return to HashRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store/store';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './components/app/App';
 
